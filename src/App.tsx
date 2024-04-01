33bfd182fab8f45f790db567f8cc611991494c39
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-01 13:39:57 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,89 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
+
+        <ul className="navbar-nav flex-row d-md-none">
+          <li className="nav-item text-nowrap">
+            <button className="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
+              <svg className="bi"></svg>
+            </button>
+          </li>
+          <li className="nav-item text-nowrap">
+            <button className="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+              <svg className="bi"></svg>
+            </button>
+          </li>
+        </ul>
+
+        <div id="navbarSearch" className="navbar-search w-100 collapse">
+          <input className="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search" />
+        </div>
       </header>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+            <div className="offcanvas-md offcanvas-end bg-body-tertiary" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+              </div>
+              <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+
+                <ul className="nav flex-column mb-auto">
+                  <li className="nav-item">
+                    <a className="nav-link d-flex align-items-center gap-2" href="#">
+                      <svg className="bi"></svg>
+                      Settings
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link d-flex align-items-center gap-2" href="#">
+                      <svg className="bi"></svg>
+                      Sign out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+            <div className="table-responsive small">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
