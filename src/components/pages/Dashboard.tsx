e8287bd147c9b23c3654f07ea3834e3955e55459
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-01 14:50:35 +0100

    Add wrapper components

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Wrapper from '../Wrapper'
 
-class Dashboard extends Component{
-    render(){
+class Dashboard extends Component {
+    render() {
         return (
-            <div>
-                Dashboard
-            </div>
+            <Wrapper>
+                <div>
+                    Dashboard
+                </div>
+            </Wrapper>
         )
     }
 }
