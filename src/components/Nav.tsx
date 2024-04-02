d8b98c016e5a1a568c9349ed644e0b3bac68cf74
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-02 01:47:23 +0100

    Display user name in dashboard page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,33 @@
-import React, { Component } from "react";
+import axios, { AxiosResponse } from "axios";
+import React, { Component, useEffect, useState } from "react";
 
-class Nav extends Component {
-    render(): React.ReactNode {
-        return (
-            <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
-                <input className='from-control form-control-dark w-100' type="text" placeholder='search' aria-label='Search' />
-                <ul className='navbar-nav px-3'>
-                    <li className='="nav-item text-nowrap'>
-                        <a className="nav-link" href='a'>Sign out</a>
-                    </li>
-                </ul>
+const Nav = () => {
+    const [user, setUser] = useState({
+        first_name: '',
+        last_name: '',
+        email: ''
+    })
 
-                <div id="navbarSearch" className="navbar-search w-100 collapse">
-                    <input className="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search" />
-                </div>
-            </header>
-        )
-    }
+    useEffect(() =>{
+        axios.get('http://localhost:8000/api/user', {withCredentials: true}).then((res: AxiosResponse) =>{
+            setUser(res.data)
+        }).catch((e)=> {
+            console.log(e)
+        })
+    }, [])
+
+    return (
+        <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
+            <input className='from-control form-control-dark w-100' type="text" placeholder='search' aria-label='Search' />
+            <ul className='navbar-nav px-3'>
+                <li className='="nav-item text-nowrap'>
+                    <a className="p-2 text-white text-decoration-none" href='#'>{user?.first_name}</a>
+                    <a className="p-2 text-white text-decoration-none" href='#'>Sign out</a>
+                </li>
+            </ul>
+        </header>
+    )
 }
 
 export default Nav; 
