5ce7f295ef90f40605befe323db733035d9c9a7b
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-02 02:47:50 +0100

    Add navlink to disable active link

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,36 @@
 import React from "react"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (<div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-        <div className="offcanvas-md offcanvas-end bg-body-tertiary" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
-            <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+    return (
+        <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+            <div className="offcanvas-md offcanvas-end bg-body-tertiary" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+                </div>
+                <nav id="sidebarMenu">
+                    <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <NavLink to={'/'} className={({ isActive, isPending }) =>
+                                    isPending ? "nav-link active" : isActive ? "nav-link disabled" : "nav-link"
+                                } >
+                                    Dashboard
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={'/users'} className={({ isActive, isPending }) =>
+                                    isPending ? "nav-link active" : isActive ? "nav-link disabled" : "nav-link"
+                                } >
+                                    Users
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-            <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-                <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <Link to={'/'} className="nav-link d-flex align-items-center gap-2 active" aria-current="page">
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={'/users'} className="nav-link d-flex align-items-center gap-2 active" aria-current="page">
-                            Users
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>)
+        </div>)
 }
 
 export default Menu;
