5cd8d00d5652c603a626b3f782671c3d8902d58d
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-05 00:49:40 +0100

    Add export's function to csv file

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -37,11 +37,21 @@ const Orders = () => {
         setSelected(selected === id ? 0: id)
     }
 
+    const handleExport = async () =>{
+        const {data} = await axios.post('export', {responseType: 'blob'})
+        const blob = new Blob([data], {type: 'text/csv'})
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'orders.csv'
+        link.click();
+    }
+
 
     return (
         <Wrapper>
             <div className="pt-3 pb-2 mb-3 border-bottom">
-                <Link to={"/orders/create"} className="btn btn-sm btn-outline-secondary">Add</Link>
+                <a href='#' className="btn btn-sm btn-outline-secondary" onClick={handleExport}>Export</a>
             </div>
             <div className="table-responsive small">
                 <table className="table table-striped table-sm">
