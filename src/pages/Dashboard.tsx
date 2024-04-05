f959b9bfd435b8eb173cf9a8d5bb21365a8da43c
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-04-05 01:09:17 +0100

    Add chart with data get from backend

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,55 @@
-import React, { Component } from 'react'
-import Wrapper from '../components/Wrapper'
+import React, { Component, useEffect } from 'react'
+import Wrapper from '../components/Wrapper';
+import * as c3 from 'c3'
+import axios from 'axios';
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <Wrapper>
-                <div>
-                    Dashboard
-                </div>
-            </Wrapper>
-        )
-    }
+const Dashboard = () => {
+    useEffect(() =>{
+        (
+            async()=>{
+                const chart = c3.generate({
+                    bindto:'#chart',
+                    data: {
+                        x: 'x',
+                        columns: [
+                            ["x"],
+                            ['sales']
+                        ],
+                        types: {
+                            Sales: 'bar'
+                        }
+                    },
+                    axis: {
+                        x: {
+                            type: 'timeseries',
+                            tick: {
+                                format: '%y-%m-%d'
+                            }
+                        }
+                    }
+                });
+
+                const {data} = await axios.get('chart');
+                console.log(data);
+
+                chart.load({
+                    columns:[
+                        ['x', ...data.map((r: any) => r.date) ],
+                        ['Sales', ...data.map((r: any) => r.sum)]
+                    ]
+                })
+            }
+        )()
+
+    }, [])
+    
+    return (
+        <Wrapper>
+            <h2>Daily Sales</h2>
+
+            <div id='chart' />
+        </Wrapper>
+    )
 }
 
 export default Dashboard;
